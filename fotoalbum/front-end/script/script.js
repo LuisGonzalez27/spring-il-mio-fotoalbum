8e877d1d5915829e48b745a8177f194ad6a8c102
LuisGonzalez27 <luis27coding@gmail.com>
2023-04-17 14:14:12 +0200

    Changes to the front-end

--- fotoalbum/front-end/script/script.js
+++ fotoalbum/front-end/script/script.js
@@ -7,7 +7,7 @@ const CONTACT_URL = 'http://localhost:8080/api/v1/contacts/create';
 const contactForm = document.getElementById('contact-form');
 
 //API
-const getPhoto = async() => {
+const getPhoto = async () => {
     const response = await fetch(PHOTO_URL);
     console.log(response);
     return response;
@@ -17,15 +17,20 @@ const getPhoto = async() => {
 //DOM
 const createPhotoItem = (item) => {
     return `
-    <div class="col-4"> 
-                <div class="card h-100">
-                    <img src="${item.url}" class="card-img-top" alt="${item.title}">
-                    <div class="card-body">
-                        <h5 class="card-title">${item.title}</h5>
-                        <p class="card-text">${item.description}</p>
-                    </div>
+    <div class="col-12 col-xxl-3 col-lg-4 col-md-6"> 
+        <div class="flip-card">
+            <div class="flip-card-inner">
+                <div class="flip-card-front">
+                    <img src="${item.url}" alt="${item.title}">
                 </div>
-    </div>`;
+                <div class="flip-card-back">
+                    <p class="title">${item.title}</p>
+                    <p>${item.description}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
 };
 
 const createPhotoList = (data) => {
@@ -33,7 +38,7 @@ const createPhotoList = (data) => {
 
     let list = ` <h1>Photo album</h1>
     <div class="row gy-4">`;
-    
+
     data.forEach((element) => {
         list += createPhotoItem(element);
     });
@@ -43,11 +48,11 @@ const createPhotoList = (data) => {
 };
 
 
-const loadData = async() => {
+const loadData = async () => {
     const response = await getPhoto();
     //console.log(response);
 
-    if(response.ok){
+    if (response.ok) {
         //get data
         const data = await response.json();
         //data filtered for visibility in the front end
@@ -62,16 +67,16 @@ const loadData = async() => {
     }
 };
 
-function filterByTitle(photo){
+function filterByTitle(photo) {
     const inputSearch = document.getElementById('search-title');
     const inputSearchValue = inputSearch.value;
 
     if (inputSearchValue) {
         const filterPhoto = photo.filter((element) =>
-        element.title.toLowerCase().includes(inputSearchValue.toLowerCase())
-      );
-      console.log(filterPhoto );
-      return filterPhoto.length > 0 ? filterPhoto : null;
+            element.title.toLowerCase().includes(inputSearchValue.toLowerCase())
+        );
+        console.log(filterPhoto);
+        return filterPhoto.length > 0 ? filterPhoto : null;
 
     } else {
         loadData();
@@ -88,7 +93,11 @@ formSearch.addEventListener("submit", async (event) => {
     if (filterList) {
         contentElement.innerHTML = createPhotoList(filterList);
     } else {
-        contentElement.innerHTML = `<h1>Sorry, there are no results available.</h1>`;
+        contentElement.innerHTML = `
+        <div class="alert alert-info mt-5" role="alert">
+            Sorry, there are no results available!
+        </div>
+        `;
     }
 });
 
@@ -98,9 +107,9 @@ const postContact = async (jsonData) => {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-    },
-    body: jsonData,  
-      
+        },
+        body: jsonData,
+
     };
     const response = await fetch(CONTACT_URL, fetchOptions);
     return response;
@@ -110,24 +119,31 @@ const saveContact = async (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const formDataObj = Object.fromEntries(formData.entries());
-    
+
     const formDataJson = JSON.stringify(formDataObj);
 
     const response = await postContact(formDataJson);
-    
+
     const responseBody = await response.json();
 
     if (response.ok) {
-      loadData();
-      event.target.reset();
+        loadData();
+        contactForm.innerHTML = `
+        <meta http-equiv="refresh" content="6;url=/index.html"/>
+        <div class="alert alert-success" role="alert">
+        The message has been successfully sent.
+        Redirect to homepage in 6 seconds...
+        </div>
+        `;
     } else {
-      console.log('ERROR');
-      console.log(response.status);
-      console.log(responseBody);
+        console.log('ERROR');
+        console.log(response.status);
+        console.log(responseBody);
     }
 };
 
 //Global code
-
+if (contactForm) {
+    contactForm.addEventListener("submit", saveContact);
+};
 loadData();
-contactForm.addEventListener("submit", saveContact);
